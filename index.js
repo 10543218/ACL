633a27074851e3ed85ff0411ed86ec8ca22047b7
10543218 <53287639+10543218@users.noreply.github.com>
2020-03-27 08:21:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var helloText1 = "大家好\n歡迎大家一起學習ACL\n請不要把本文章的全部內容都複製下來自己使用";
+var helloText2 = "\n筆者做這些花了蠻多的時間與心血\n謝謝大家的配合";
+var helloText3 = "\n\n如果想要再新增什麼功能\n歡迎跟筆者聯絡~";
+alert(helloText1+helloText2+helloText3);
+
 // 顯示左側選單
 var menuIsOpen = false;
 var burgerIsClick = false;
