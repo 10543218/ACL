338fdfeecc56e52b17088238d15dd6c4bfd52642
10543218 <53287639+10543218@users.noreply.github.com>
2020-03-31 18:35:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var helloText1 = "大家好\n歡迎大家一起學習ACL\n請不要把本文章的全部內容都複製下來自己使用";
-var helloText2 = "\n筆者做這些花了蠻多的時間與心血\n謝謝大家的配合";
+var helloText2 = "\n筆者做這些花了蠻多的時間與心血\n謝謝大家的配合\n\n藍色的字都是超連結!";
 var helloText3 = "\n\n如果想要再新增什麼功能\n歡迎跟筆者聯絡~";
 alert(helloText1+helloText2+helloText3);
 
@@ -24,6 +24,8 @@ function closeMenu(){
 	burger.click();
 }
 
+
+// 顯示答案用
 function showAnsOne(){
 	var answerOne = document.getElementById("answerOne");
 	answerOne.style.visibility = "visible";	
@@ -37,4 +39,18 @@ function showAnsTwo(){
 function showAnsThree(){
 	var answerThree = document.getElementById("answerThree");
 	answerThree.style.visibility = "visible";	
+}
+
+
+// 連結網頁用
+function toIndexPage(){
+	window.location.href = "index.html";
+}
+
+function to0324Page(){
+	window.location.href = "0324.html";
+}
+
+function to0331Page(){
+	window.location.href = "0331.html";
 }
